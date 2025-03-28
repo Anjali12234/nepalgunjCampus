e523d84399588304ee63b48dc0609267f65329f4
anjali12234 <anjisharma649@gmail.com>
2025-03-28 12:09:08 +0545

    margin adjusted

--- resources/views/frontend/allGallery.blade.php
+++ resources/views/frontend/allGallery.blade.php
@@ -8,12 +8,15 @@
     <div class="container mx-auto px-4 py-8">
 
         <div class="overflow-hidden rounded-lg shadow-md">
-            <div class="mt-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            <div class="mt-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 mx-5">
                     @foreach ($galleries as $gallery)
-                    <a href="{{ route('galleryList',$gallery) }}" target="_blank">
-                        <img src="{{ $gallery->image }}" alt="Gallery Image"
-                            class="w-full h-60 shadow-xl shadow-neutral-900 object-cover transition-transform duration-300 hover:scale-105">
-                    </a>
+                    <div class="mb-5 mt-5">
+
+                        <a href="{{ route('galleryList',$gallery) }}" >
+                            <img src="{{ $gallery->image }}" alt="Gallery Image"
+                                class="w-full h-60 shadow-xl shadow-neutral-900 object-cover transition-transform duration-300 hover:scale-105">
+                        </a>
+                    </div>
                     @endforeach
                 </div>
             </div>
