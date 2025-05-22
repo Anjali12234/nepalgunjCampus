86f81727bc103a36be5839717a19190b2dff78e3
anjali12234 <anjisharma649@gmail.com>
2025-05-22 14:21:40 +0545

    changes on the site

--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -19,7 +19,7 @@
 
                     <div class="mt-12 sm:mt-8 text-gray-800">
                         <h1 class="text-3xl sm:text-2xl">CEH Class Registration Form</h1>
-                        <p class="text-lg mt-5 sm:mt-3"> 
+                        <p class="text-lg mt-5 sm:mt-3">
                             <span>Get All time support for emergency.</span>
                         </p>
                         <form action="{{ route('registrationForm.submit') }}" method="POST" enctype="multipart/form-data">
@@ -35,13 +35,13 @@
                             @endif
                             <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-5">
                                 <div>
-                                    <input type="text" name="name" value="{{old('name')}}"
+                                    <input type="text" name="name" value="{{ old('name') }}"
                                         class="py-3 px-4 w-full border-gray-200 rounded-lg text-lg focus:ring-2 
                                         focus:ring-blue-500 focus:outline-none"
                                         placeholder="Your Name *">
                                 </div>
                                 <div>
-                                    <input type="text" name="email" value="{{old('email')}}"
+                                    <input type="text" name="email" value="{{ old('email') }}"
                                         class="py-3 px-4 w-full border-gray-200 rounded-lg text-lg focus:ring-2 
                                         focus:ring-blue-500 focus:outline-none"
                                         placeholder="Your Email *">
@@ -49,13 +49,13 @@
                             </div>
                             <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-5">
                                 <div>
-                                    <input type="text" name="phone_no" value="{{old(key: 'phone_no')}}"
+                                    <input type="text" name="phone_no" value="{{ old(key: 'phone_no') }}"
                                         class="py-3 px-4 w-full border-gray-200 rounded-lg text-lg focus:ring-2 
                                         focus:ring-blue-500 focus:outline-none"
                                         placeholder="Your Phone Number *">
                                 </div>
                                 <div>
-                                    <input type="text" name="address" value="{{old('address')}}"
+                                    <input type="text" name="address" value="{{ old('address') }}"
                                         class="py-3 px-4 w-full border-gray-200 rounded-lg text-lg focus:ring-2 
                                         focus:ring-blue-500 focus:outline-none"
                                         placeholder="Your Address *">
@@ -63,17 +63,25 @@
                             </div>
                             <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-5">
                                 <div>
-                                    <input type="text" name="college_name" value="{{old('college_name')}}"
+                                    <input type="text" name="college_name" value="{{ old('college_name') }}"
                                         class="py-3 px-4 w-full border-gray-200 rounded-lg text-lg focus:ring-2 
                                         focus:ring-blue-500 focus:outline-none"
                                         placeholder="Your College Name of +2 *">
                                 </div>
                                 <div>
-                                    <input type="text" name="faculty" value="{{old('faculty')}}"
+                                    <select name="faculty"
                                         class="py-3 px-4 w-full border-gray-200 rounded-lg text-lg focus:ring-2 
-                                        focus:ring-blue-500 focus:outline-none"
-                                        placeholder="Faculty *">
+               focus:ring-blue-500 focus:outline-none">
+                                        <option value="">Select Faculty *</option>
+                                        <option value="Science" {{ old('faculty') == 'Science' ? 'selected' : '' }}>Science
+                                        </option>
+                                        <option value="Management" {{ old('faculty') == 'Management' ? 'selected' : '' }}>
+                                            Management</option>
+                                        <option value="Arts" {{ old('faculty') == 'Arts' ? 'selected' : '' }}>Arts
+                                        </option>
+                                    </select>
                                 </div>
+
                             </div>
                             <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-5">
                                 <div>
@@ -91,9 +99,9 @@
                                 </div>
                             </div>
                             <div class="mt-5">
-                                <textarea name="message" 
+                                <textarea name="message"
                                     class="py-3 px-4 w-full border-gray-200 rounded-lg text-lg focus:ring-2 focus:ring-blue-500 focus:outline-none"
-                                    placeholder="Your Message*">{{old('message')}}</textarea>
+                                    placeholder="Your Message*">{{ old('message') }}</textarea>
                                 <div class="mt-4">
                                     <input type="submit" value="Submit"
                                         class="bg-[#FFC315] text-neutral-800 font-bold px-12 py-3 rounded-full hover:bg-white 
@@ -104,7 +112,7 @@
                     </div>
                     <div class="mt-44 sm:mt-24 text-gray-800">
                         <h1 class="text-3xl sm:text-2xl">Make a Contact</h1>
-                        <p class="text-lg mt-5 sm:mt-3"> 
+                        <p class="text-lg mt-5 sm:mt-3">
                             <span>Get All time support for emergency.</span>
                             <span class="text-red-600">Get Connected with us for any urgency.</span>
                         </p>
@@ -161,52 +169,52 @@
                     </div>
 
                     <div class="mt-16 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
-                            <div
-                                class="flex flex-col items-center group bg-stone-100 border shadow-sm hover:shadow-lg transition 
+                        <div
+                            class="flex flex-col items-center group bg-stone-100 border shadow-sm hover:shadow-lg transition 
                                 h-80 sm:h-96 p-6">
-                                <div
-                                    class="relative w-36 h-36 rounded-full overflow-hidden border-8 border-white group-hover:border-[#FFC315] shadow-md">
-                                    <img class="w-full h-full object-cover group-hover:scale-110 transition-transform"
-                                        src="{{ asset('assets/frontend/images/location.jpg') }}" alt="Card Image">
-                                </div>
-                                <div class="p-4 text-center mt-6">
-                                    <h3 class="text-2xl text-gray-800">Address:</h3>
-                                    <p class="mt-2 text-base">
-                                        {{ systemSetting()?->address}}
-                                    </p>
-                                </div>
-                            </div>
                             <div
-                                class="flex flex-col items-center group bg-stone-100 border shadow-sm hover:shadow-lg transition 
-                                h-80 sm:h-96 p-6">
-                                <div
-                                    class="relative w-36 h-36 rounded-full overflow-hidden border-8 border-white group-hover:border-[#FFC315] shadow-md">
-                                    <img class="w-full h-full object-cover group-hover:scale-110 transition-transform"
-                                        src="{{ asset('assets/frontend/images/location.jpg') }}" alt="Card Image">
-                                </div>
-                                <div class="p-4 text-center mt-6">
-                                    <h3 class="text-2xl text-gray-800">Email:</h3>
-                                    <p class="mt-2 text-base">
-                                        {{ systemSetting()?->email}}
-                                    </p>
-                                </div>
+                                class="relative w-36 h-36 rounded-full overflow-hidden border-8 border-white group-hover:border-[#FFC315] shadow-md">
+                                <img class="w-full h-full object-cover group-hover:scale-110 transition-transform"
+                                    src="{{ asset('assets/frontend/images/location.jpg') }}" alt="Card Image">
                             </div>
+                            <div class="p-4 text-center mt-6">
+                                <h3 class="text-2xl text-gray-800">Address:</h3>
+                                <p class="mt-2 text-base">
+                                    {{ systemSetting()?->address }}
+                                </p>
+                            </div>
+                        </div>
+                        <div
+                            class="flex flex-col items-center group bg-stone-100 border shadow-sm hover:shadow-lg transition 
+                                h-80 sm:h-96 p-6">
                             <div
-                                class="flex flex-col items-center group bg-stone-100 border shadow-sm hover:shadow-lg transition 
-                                h-80 sm:h-96 p-6">
-                                <div
-                                    class="relative w-36 h-36 rounded-full overflow-hidden border-8 border-white group-hover:border-[#FFC315] shadow-md">
-                                    <img class="w-full h-full object-cover group-hover:scale-110 transition-transform"
-                                        src="{{ asset('assets/frontend/images/location.jpg') }}" alt="Card Image">
-                                </div>
-                                <div class="p-4 text-center mt-6">
-                                    <h3 class="text-2xl text-gray-800">Phone Number:</h3>
-                                    <p class="mt-2 text-base">
-                                        {{ systemSetting()?->phone_number}},
-                                        {{ systemSetting()?->telephone_number}}
-                                    </p>
-                                </div>
+                                class="relative w-36 h-36 rounded-full overflow-hidden border-8 border-white group-hover:border-[#FFC315] shadow-md">
+                                <img class="w-full h-full object-cover group-hover:scale-110 transition-transform"
+                                    src="{{ asset('assets/frontend/images/location.jpg') }}" alt="Card Image">
                             </div>
+                            <div class="p-4 text-center mt-6">
+                                <h3 class="text-2xl text-gray-800">Email:</h3>
+                                <p class="mt-2 text-base">
+                                    {{ systemSetting()?->email }}
+                                </p>
+                            </div>
+                        </div>
+                        <div
+                            class="flex flex-col items-center group bg-stone-100 border shadow-sm hover:shadow-lg transition 
+                                h-80 sm:h-96 p-6">
+                            <div
+                                class="relative w-36 h-36 rounded-full overflow-hidden border-8 border-white group-hover:border-[#FFC315] shadow-md">
+                                <img class="w-full h-full object-cover group-hover:scale-110 transition-transform"
+                                    src="{{ asset('assets/frontend/images/location.jpg') }}" alt="Card Image">
+                            </div>
+                            <div class="p-4 text-center mt-6">
+                                <h3 class="text-2xl text-gray-800">Phone Number:</h3>
+                                <p class="mt-2 text-base">
+                                    {{ systemSetting()?->phone_number }},
+                                    {{ systemSetting()?->telephone_number }}
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
